6b781c2a97515112d80c982b9b8477afb6e5fee0
njdevengine <newjerseydevelopment@gmail.com>
2019-04-04 14:03:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,24 @@
+#import dependencies
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
+from flask import request, Flask, jsonify
 #################################################
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
 Base.prepare(engine, reflect=True)
-
-# Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
-# Create our session (link) from Python to the DB
 session = Session(engine)
-##################################
-# Measurement table 
-# id INTEGER
-# station TEXT
-# date TEXT
-# prcp FLOAT
-# tobs FLOAT
-
-# Station table 
-# id
-# station
-# name
-# latitude
-# longitude
-# elevation
-##################################
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
 #################################################
 # Flask Routes
 #################################################
@@ -54,92 +28,117 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
-    )
+        f"<a href= \"/api/v1.0/precipitation\">Route 1: date & prcp</a><br/>"
+        f"<a href= \"/api/v1.0/stations\">Route 2: station list</a><br/>"
+        f"<a href= \"/api/v1.0/tobs\">Route 3: last year's temp observations & dates</a><br/>"
+        f"<a href=\"/api/v1.0/start?s=2017-01-01\">min, avg, max from start date</a><br/>"
+        f"<a href=\"/api/v1.0/start_end?s=2017-01-01&e=2018-01-01\">min, avg, max in range</a><br/>"           
+           )
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    return(f"working")
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
+    session = Session(engine)
+    array = []
+    x = session.query(Measurement).all()
+    for i in x:
+        prcp_dict = {}
+        prcp_dict[i.date] = i.prcp
+        array.append(prcp_dict)
+    return jsonify(array)
+# Measurement table:
+################
+# id
+# station
+# date
+# prcp
+# tobs
 
 @app.route("/api/v1.0/stations")
 def stations():
-    return(f"working")
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
+    session = Session(engine)
+    array = []
+    x = session.query(Station).all()
+    for i in x:
+        stat_dict = {}
+        stat_dict["elevation"]  = i.elevation
+        stat_dict["lon"]  = i.longitude
+        stat_dict["lat"]  = i.latitude
+        stat_dict["name"]  = i.name
+        stat_dict["station"] = i.station
+        array.append(stat_dict)
+    return jsonify(array)
+# Station table:
+################
+# station
+# name
+# latitude
+# longitude
+# elevation
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return(f"working")
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
+    session = Session(engine)
+    x = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date.between('2016-08-23', '2017-08-23')).all()
+    tobs_dict = {}
+    for i in x:
+        tobs_dict[i.tobs] = i.date
+    return jsonify(tobs_dict)
+# Measurement table:
+################
+# id
+# station
+# date
+# prcp
+# tobs
 
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/start")
 def start():
-    return(f"working")
+    s = request.args.get('s')
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
+    session = Session(engine)
+    x = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= s).all()
+    start_dict = {}
+    for i in x:
+        start_dict["min"] = x[0][0]
+        start_dict["avg"] = x[0][1]
+        start_dict["max"] = x[0][2]
+    return jsonify(start_dict)
 
-@app.route("/api/v1.0/<start>/<end>")
-def startEnd():
-    return(f"working")
+@app.route("/api/v1.0/start_end")
+def start_and_end():
+    s = request.args.get('s')
+    e = request.args.get('e')
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
+    session = Session(engine)
+    x = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date.between(s,e)).all()
+    se_dict = {}
+    se_dict["min"] = x[0][0]
+    se_dict["avg"] = x[0][1]
+    se_dict["max"] = x[0][2]    
+    return jsonify(se_dict)
 
 if __name__ == "__main__":
     app.run(debug=False)
-
-# @app.route("/api/v1.0/precipitation")
-# def prcp():
-#     """Return a list of all precipitation"""
-#     # Query all precipitation
-#     results = session.query(Measurement.prcp).all()
-#     all_prcp = []
-#     for prcp in results:
-#         prcp_dict = {}
-#         prcp_dict[Measurement.date] = Measurement.prcp
-#         all_prcp.append(prcp_dict)
-
-# #     all_prcp = list(np.ravel(results))
-
-#     return jsonify(all_prcp)
-
-
-# @app.route("/api/v1.0/stations")
-# def stations():
-#     """Return a list of stations"""
-#     # Query all passengers
-#     results = session.query(Station).all()
-#     # Create a dictionary from the row data and append to a list of all_stations
-#     all_stations = []
-#     for station in results:
-#         station_dict = {}
-#         station_dict["id"] = Station.id
-#         station_dict["station"] = Station.station
-#         station_dict["name"] = Station.name
-#         station_dict["latitude"] = Station.latitude
-#         station_dict["longitude"] = Station.longitude
-#         station_dict["elevation"] = Station.elevation
-#         all_stations.append(station_dict)
-
-#     return jsonify(all_stations)
-
-# if __name__ == '__main__':
-#     app.run(debug=False)
-
-- - -
-
-# ## Step 2 - Climate App
-
-# * `/api/v1.0/precipitation`
-#   * Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
-#   * Return the JSON representation of your dictionary.
-
-# * `/api/v1.0/stations`
-#   * Return a JSON list of stations from the dataset.
-
-# * `/api/v1.0/tobs`
-#   * query for the dates and temperature observations from a year from the last data point.
-#   * Return a JSON list of Temperature Observations (tobs) for the previous year.
-
-# * `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
-#   * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-#   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-#   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-
-# ## Hints
-# * You will need to join the station and measurement tables for some of the analysis queries.
